4ff60d752bf5a19cb99b49a06f075c34bbe21d5d
菜菜 <3104591385@qq.com>
2024-10-18 12:08:54 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import pagination from "./components/pagination.vue";
+import { reactive, ref } from "vue";
+
+
+const globalData = reactive({ componentName: "分页列表-zion", });
+const setGlobalData = (obj: any) => {
+  globalData.componentName = obj.value;
+};
+
+
+// const url = ref("");
+// const actionflow_id = ref("5e5e155d-7b3c-4771-bf94-ceb597cbb666");
+// const where = ref("");
+// const limit = ref(-1);
+// const table_name = ref("");
+
+const url = ref("");
+
+
+const actionflow_id = ref("");
+const where = ref("");
+const limit = ref();
+const table_name = ref("");
+
+
+const redirectUrl=""
+const pageFiled=""
+const pageId=""
+
+
+
+</script>
+<template>
+  <pagination
+    :where="where"
+    :limit="limit"
+    :table_name="table_name"
+    :url="url"
+    :pageFiled="pageFiled"
+    :pageId="pageId"
+    :redirectUrl="redirectUrl"
+    :actionflow_id="actionflow_id"
+    :global-data="globalData"
+    :set-global-data="setGlobalData"
+  />
+
+</template>
+
+<style>
+#app {
+  /* height: 60vh; */
+  /* background-color: #ffffff; */
+}
+</style>
---
+++ src/components/pagination.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-wrap" >
+    123123
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+@import url("//unpkg.com/element-ui@2.13.2/lib/theme-chalk/index.css");
+
+* {
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+
+</style>
